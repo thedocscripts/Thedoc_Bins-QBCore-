bc8a04deecba612c1d96aca334fd82146f9d2a43
Thedoc Scripts <82754205+thedocscripts@users.noreply.github.com>
2022-04-03 21:08:00 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,20 +2,13 @@ fx_version 'cerulean'
 games { 'gta5' }
 author 'thedoc'
 
-ui_page 'ui/html/menu.html'
 
-files {
-    'ui/**'
-}
 
 client_scripts {
     'client/**',
     'utils/utils.lua'
 }
 
-server_scripts {
-    'server/**'
-}
 
 shared_scripts {
     'config.lua'
